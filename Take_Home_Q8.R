7023ec352e84d6792e1d2b598a46f6621e58272e
tomasleon <tomas.leon@berkeley.edu>
2021-02-19 07:13:40 -0800

    Update script and .csv

--- Take_Home_Q8.R
+++ Take_Home_Q8.R
@@ -1,5 +1,5 @@
 #Code for Take Home Final Q8
-#Calculate the average level of greenness within 1km of the wells that were drilled after 2013. 
+#Calculate the average level of greenness within 1km of brownfield sites designated after 2004
 #Produce 2 plots, worth 15 total points.
 #I provide the skeleton of the code, but in parts b-e you will need to add a bit to produce the final plots
 
@@ -9,13 +9,13 @@ library(tidyverse)
 #b: Bring in data
 q8 <- read_csv("ADD CSV FILE NAME")
 
-#c: Subset to wells drilled after 2013 (i.e., 2014-)
+#c: Subset to brownfield sites designated after 2004 (i.e., 2005-)
 q8_subset <- filter(q8, "ADD APPROPRIATE CODE HERE")
 
-#d: Calculate average level of greenness within 1km of wells drilled after 2013
+#d: Calculate average level of greenness within 1km of brownfield sites designated after 2004
 summary(q8_subset$"ADD APPROPRIATE COLUMN HERE")
 
-#e: Produce two plots illustrating average NDVI within 1km of wells drilled after 2013
+#e: Produce two plots illustrating average NDVI within 1km of brownfield sites designated after 2004
 ggplot(q8_subset, aes(x = as.factor(year), y = "ADD APPROPRIATE COLUMN NAME HERE")) + 
   geom_violin() +
   xlab("ADD X LABEL") +
